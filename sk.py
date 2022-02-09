18680200ce4b7cadb182ca1a7ed09341774d7309
camopov <96944108+camopov@users.noreply.github.com>
2022-02-09 21:59:51 +0100

    Add files via upload

---
+++ sk.py
@@ -0,0 +1,12 @@
+stromkosten = input("Bitte geben sie ihre Stromkosten pro KWh, in Euro, an\n")
+stromverbrauch = input("Bitte geben sie den Durchschnittlichen Stromverbrauch des Geräts, in Watt, an\n")
+zeit = input("Bitte geben sie in Stunden an, wielange das Gerät pro Tag läuft\n")
+sk = float(stromkosten)
+sv = float(stromverbrauch)
+z = float(zeit)
+sv = sv / 1000
+igs = z * sk * sv
+print("Die Täglichen Stromkosten des Geräts betragen " + str(igs) + " Euro")
+print("Die Wöchenlichen Stromkosten des Geräts betragen " + str(igs * 7) + " Euro")
+print("Die Monatlichen Stromkosten des Geräts betragen " + str(igs * 30) + " Euro")
+print("Die Jährlichen Stromkosten des Geräts betragen " + str(igs * 356) + " Euro")
